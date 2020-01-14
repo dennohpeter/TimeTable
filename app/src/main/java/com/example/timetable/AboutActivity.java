9331d8371763a9493912a6f6667a799abc277ba3
Dennis Peterson <denispeterson96@gmail.com>
2020-01-14 12:56:07 +0300

    updated about layout view

--- app/src/main/java/com/example/timetable/AboutActivity.java
+++ app/src/main/java/com/example/timetable/AboutActivity.java
@@ -1,4 +1,4 @@
-package com.example.tabbedactivity;
+package com.example.timetable;
 
 import android.content.Context;
 import android.content.Intent;
@@ -7,12 +7,22 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 
 public class AboutActivity extends AppCompatActivity {
@@ -25,24 +35,13 @@ public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Handles shared preferences
-        sharedPreferences = getSharedPreferences(myPreference,
-                Context.MODE_PRIVATE);
-        if (sharedPreferences.contains(themeKey)) {
-            if (sharedPreferences.getString(themeKey, "").equals("1")) {
-                setTheme(R.style.DarkTheme);
-            }
-//            else setTheme(R.style.AppTheme);
-        } else {
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString(themeKey, "0");
-            editor.apply();
-        }
-        //Sets the main activity layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Toolbar toolbar = findViewById(R.id.about_toolbar);
         setSupportActionBar(toolbar);
+        // setting back button on toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         // setting app version
         version = findViewById(R.id.app_version);
@@ -89,4 +88,37 @@ public class AboutActivity extends AppCompatActivity {
 
     }
 
+    public void showCredit(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.credits);
+
+        View popupLayout = getLayoutInflater().inflate(R.layout.contributor_list_view, null);
+        HashMap<String, String> contributor_roles = new HashMap<>();
+        contributor_roles.put(getString(R.string.creator), getString(R.string.creator_roles));
+        contributor_roles.put(getString(R.string.contributor1), getString(R.string.developer1_roles));
+        contributor_roles.put(getString(R.string.contributor2), getString(R.string.developer2_roles));
+
+        List<HashMap<String, String>> listItem = new ArrayList<>();
+
+        ListView listView = popupLayout.findViewById(R.id.list);
+
+        SimpleAdapter adapter = new SimpleAdapter(this,listItem,R.layout.contributor_list_item,
+                new String[]{"name", "role"},
+                new int[]{R.id.contributor_name, R.id.developer_role});
+        for (Map.Entry<String, String> pair : contributor_roles.entrySet()) {
+            HashMap<String, String> resultMap = new HashMap<>();
+            resultMap.put("name", ((Map.Entry) pair).getKey().toString());
+            resultMap.put("role", ((Map.Entry) pair).getValue().toString());
+            listItem.add(resultMap);
+        }
+        listView.setAdapter(adapter);
+
+        builder.setView(popupLayout);
+        builder.setNegativeButton(R.string.ok, (dialog, which) -> dialog.dismiss());
+        builder.create().show();
+    }
+
+    public void rateApp(View view) {
+        Toast.makeText(this, "Rate this app", Toast.LENGTH_SHORT).show();
+    }
 }
